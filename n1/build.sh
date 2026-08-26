2a53675723b9a334f23b0c0b86169747b80c5d28
iliililil <6303366@gmail.com>
2026-08-26 18:20:48 +0800

    Update build.sh

--- n1/build.sh
+++ n1/build.sh
@@ -52,7 +52,7 @@ esac
 # daed 代理
 case "$PLUGINS" in
     *daed*)
-        add_plugin_group "iliililil/CloudRunFilesBuilder#daed" "daed.run"
+        add_plugin_group "iliililil/CloudRunFilesBuilder#luci-app-daed" "luci-app-daed.run"
         echo "✅ 已选择 daed 组件"
         ;;
 esac
