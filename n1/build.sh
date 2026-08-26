ee012bd756c968cc927c1d412e0a8a9c2a5620c6
iliililil <6303366@gmail.com>
2026-08-26 16:52:52 +0800

    Remove AdGuardHome plugin from build.sh
    Removed AdGuardHome plugin from the build script.

--- n1/build.sh
+++ n1/build.sh
@@ -38,7 +38,6 @@ add_plugin_group() {
 # 晶晨宝盒（写入EMMC/内核管理 必备）
 add_plugin_group "ophub/luci-app-amlogic" "luci-app-amlogic luci-i18n-amlogic-zh-cn"
 add_plugin_group "iliililil/CloudRunFilesBuilder#quickfile" "quickfile.run"
-add_plugin_group "iliililil/CloudRunFilesBuilder#AdGuardHome" "AdGuardHome.run"
 
 
 # iStore 应用商店
